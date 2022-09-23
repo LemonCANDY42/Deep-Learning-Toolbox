d10194147bd6aef45a1704e096337581848f060b
Kenny Zhou <l.w.r.f.42@icloud.com>
2022-09-23 14:11:18 +0800

    update unit augmentation

--- DataAugmentation/visualization.py
+++ DataAugmentation/visualization.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 
-def unit_augmentation(path,factor):
+def unit_augmentation_factor(path,factor):
     # transform = A.Compose([
     #     A.ColorJitter(p=1,always_apply=True,hue=0.2),
     # ])
@@ -25,17 +25,30 @@ def unit_augmentation(path,factor):
 
     return adjust_hue_torchvision(image,factor)
 
+def unit_augmentation(path):
+    transform = A.Compose([
+        A.ColorJitter(brightness=0.2, contrast=0.15, saturation=0.2, hue=0.05, always_apply=False, p=0.8),
+    ])
+
+    pillow_image = Image.open(path)
+    image = np.array(pillow_image)
+    transformed = transform(image=image)
+    transformed_image = transformed["image"]
+
+    return transformed_image
+
 
 if __name__ == "__main__":
-    base_factor = 0.1
+
+    # base_factor = 0.1
     path = Path("/Users/kennymccormick/Downloads/下午/JiTest/SRC_20220920-105923-312.jpg")
     save_path = Path("/Users/kennymccormick/Downloads/下午/augmentation")
-
-    out_dir = save_path / str(path.stem)
-    os.mkdir(out_dir)
-    for i in range(10):
-        pic = unit_augmentation(path,base_factor*i)
-        pic = Image.fromarray(pic)
-
-        out_path = save_path / str(path.stem) / (str(path.stem) + f"_{i}.jpg")
-        pic.save(out_path)
+    # out_dir = save_path / str(path.stem)
+    # os.mkdir(out_dir)
+    pic = unit_augmentation(path)
+    # for i in range(10):
+    #     pic = unit_augmentation_factor(path,base_factor*i)
+    #     pic = Image.fromarray(pic)
+    #
+    #     out_path = save_path / str(path.stem) / (str(path.stem) + f"_{i}.jpg")
+    #     pic.save(out_path)
