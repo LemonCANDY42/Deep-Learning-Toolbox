d13b4decf14c8de8d669c85dd926e8079014b6db
Kenny Zhou <l.w.r.f.42@icloud.com>
2022-11-23 11:38:04 +0800

    Add speed measurement module on asyncReplaceText.py

--- TextHandling/asyncReplaceText.py
+++ TextHandling/asyncReplaceText.py
@@ -6,25 +6,32 @@
 # @Email    ：l.w.r.f.42@gmail.com
 
 # Notice:This script will modify the file on the source file, please pay attention to the backup to avoid unnecessary damage!
-# Need todo: pip install aiofiles
+# Need todo: pip install loguru
+# aiofiles,atomicwrites
 # load many files concurrently with asyncio in batch
 # from os import listdir
 # from os.path import join
 import asyncio
 # import aiofiles
 from TextHandling.pathTools import get_suffix_files
+from atomicwrites import atomic_write
 
 # load and return the contents of a list of file paths
 async def load_files(filepaths,need_replace_text,replace_text):
 	# load all files
 	for filepath in filepaths:
-		# open the file
-	# 	async with aiofiles.open(filepath, 'r') as handle:
-	# 		# load the contents and add to list
-	# 		data = await handle.read()
-	# 		# store loaded data
-	# 		data_list.append(data)
+		# # open the file
+		# async with aiofiles.open(filepath, 'r') as handle:
+		# 	# load the contents and add to list
+		# 	data = await handle.read()
+		# 	# store loaded data
+		# 	# data_list.append(data)
+		# async with aiofiles.open(filepath, 'w') as f:
+		# 	data = data.replace(need_replace_text, replace_text)
+		# 	print(data)
+		# 	f.write(data)
 	# return (data_list, filepaths)
+
 		text = filepath.read_text()
 		text = text.replace(need_replace_text, replace_text)
 		filepath.write_text(text)
@@ -49,12 +56,28 @@ async def main(path='./',need_replace_text='#$%^&',replace_text="l.w.r.f.42@gmai
 		# wait for the next task to complete
 		filepaths = await task
 		# process results
-		for filepath in filepaths:
-			# report progress
-			print(f'.loaded {filepath}')
-	print('Done')
+	# 	for filepath in filepaths:
+	# 		# report progress
+	# 		print(f'.loaded {filepath}')
+	# print('Done')
 
 
 # entry point
 if __name__ == '__main__':
-	asyncio.run(main("/Users/kennymccormick/WorkFolder/test_path",need_replace_text='Kenny Zhou',replace_text="l.w.r.f.42@gmail.com",suffix='txt'))
+	import time
+	from loguru import logger
+
+	logger.info("Start running!")
+	asyncio.run(main("/Users/kennymccormick/WorkFolder/test", need_replace_text='AWSEDASDFAS',
+									 replace_text="123216311231234", suffix='txt'))
+	repetitions = 1000
+	start_time = time.time()
+	for i in range(repetitions//2):
+		asyncio.run(main("/Users/kennymccormick/WorkFolder/test",need_replace_text='123216311231234',replace_text="l.w.r.f.42@gmail.com",suffix='txt'))
+		asyncio.run(main("/Users/kennymccormick/WorkFolder/test", need_replace_text='l.w.r.f.42@gmail.com',
+										 replace_text="123216311231234", suffix='txt'))
+	end_time = time.time()
+	cost_time = (end_time-start_time)/repetitions*1000
+	asyncio.run(main("/Users/kennymccormick/WorkFolder/test", need_replace_text='123216311231234',
+									 replace_text="AWSEDASDFAS", suffix='txt'))
+	logger.info("Cost time is {cost_time}ms.",cost_time=cost_time)
